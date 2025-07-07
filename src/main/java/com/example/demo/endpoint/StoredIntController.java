27e144e3e82c589079187da23dee3c45899eec1e
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-07-07 14:54:06 +0300

    fix: format code

--- src/main/java/com/example/demo/endpoint/StoredIntController.java
+++ src/main/java/com/example/demo/endpoint/StoredIntController.java
@@ -1,19 +1,18 @@
 package com.example.demo.endpoint;
 
 import com.example.demo.service.StoredIntService;
+import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @AllArgsConstructor
 @RestController
 public class StoredIntController {
-    private final StoredIntService storedIntService;
+  private final StoredIntService storedIntService;
 
-    @GetMapping("/stored-int")
-    public int getStoredInt() throws IOException {
-        return storedIntService.getStoredInt();
-    }
+  @GetMapping("/stored-int")
+  public int getStoredInt() throws IOException {
+    return storedIntService.getStoredInt();
+  }
 }
--- src/main/java/com/example/demo/service/StoredIntService.java
+++ src/main/java/com/example/demo/service/StoredIntService.java
@@ -1,34 +1,33 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-
 import java.io.*;
 import java.util.Random;
+import org.springframework.stereotype.Service;
 
 @Service
 public class StoredIntService {
 
-    public int getStoredInt() throws IOException {
-        var filePath = "/tmp/stored-int.txt";
-        var file = new File(filePath);
-        if (!file.exists()) {
-            CreateAndWriteContent(file);
-        }
-
-        return readContent(file);
+  public int getStoredInt() throws IOException {
+    var filePath = "/tmp/stored-int.txt";
+    var file = new File(filePath);
+    if (!file.exists()) {
+      CreateAndWriteContent(file);
     }
 
-    private int readContent(File file) throws IOException {
-        var reader = new BufferedReader(new FileReader(file));
-        return reader.read();
-    }
+    return readContent(file);
+  }
 
-    private void CreateAndWriteContent(File file) throws IOException {
-        var random = new Random();
-        var randomNumber = random.nextInt(100);
+  private int readContent(File file) throws IOException {
+    var reader = new BufferedReader(new FileReader(file));
+    return reader.read();
+  }
 
-        var writer = new FileWriter(file);
-        writer.write(randomNumber);
-        writer.close();
-    }
+  private void CreateAndWriteContent(File file) throws IOException {
+    var random = new Random();
+    var randomNumber = random.nextInt(100);
+
+    var writer = new FileWriter(file);
+    writer.write(randomNumber);
+    writer.close();
+  }
 }
